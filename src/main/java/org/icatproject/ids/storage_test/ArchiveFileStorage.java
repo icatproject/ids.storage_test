2bd73c6cc44638e9a39d55dfc7b0cb242f7a30f1
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2017-08-30 12:46:56 +0200

    Do not throw an exception in delete() if the file does not exist. See icatproject/ids.server#61.

--- src/main/java/org/icatproject/ids/storage_test/ArchiveFileStorage.java
+++ src/main/java/org/icatproject/ids/storage_test/ArchiveFileStorage.java
@@ -5,7 +5,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.DirectoryNotEmptyException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
@@ -53,12 +55,10 @@ public class ArchiveFileStorage implements ArchiveStorageInterface {
 		think();
 		String location = dsInfo.getInvId() + "/" + dsInfo.getDsId();
 		Path path = baseDir.resolve(location);
-		Files.delete(path);
-		path = path.getParent();
 		try {
 			Files.delete(path);
-		} catch (IOException e) {
-			// Investigation directory probably not empty
+			Files.delete(path.getParent());
+		} catch (DirectoryNotEmptyException | NoSuchFileException e) {
 		}
 	}
 
